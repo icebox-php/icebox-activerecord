054f89d854554da4bfb210c4d1fdfab66abb969b
Mahfuzur Rahman <bappy.gm@gmail.com>
2019-05-07 23:20:24 +0600

    quick fix

--- test/helpers/config.php
+++ test/helpers/config.php
@@ -58,7 +58,7 @@ ActiveRecord\Config::initialize(function($cfg)
 	if (class_exists('Log_file')) // PEAR Log installed
 	{
 		$logger = new Log_file(dirname(__FILE__) . '/../log/query.log','ident',array('mode' => 0664, 'timeFormat' =>  '%Y-%m-%d %H:%M:%S'));
-	
+
 		$cfg->set_logging(true);
 		$cfg->set_logger($logger);
 	}
@@ -69,7 +69,7 @@ ActiveRecord\Config::initialize(function($cfg)
 
 		DatabaseTest::$log = false;
 	}
-	
+
 	if ($GLOBALS['show_warnings']  && !isset($GLOBALS['show_warnings_done']))
 	{
 		if (!extension_loaded('memcache'))
